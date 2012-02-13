4bce7c1357de9a4e8fcc746cf1c73434df6f3413
Elie Rosen <erosen@eden.rutgers.edu>
2012-02-12 20:26:03 -0500

    tokens now parse into commands

--- myshell.c
+++ myshell.c
@@ -20,35 +20,37 @@ char const *name;
 command_t *func;
 } builtin_t;
 
+/* Default case for if the command is not in the dictionary */
 static int cmdDef(int argc, char *const *argv) {
-
+	printf("in default");
 	return 0;
 }
 
+/* code that performs the CD command */
 static int cmdCd(int argc, char *const *argv) {
 	char const *n = NULL;
 	int x;
 	
 	if (argc < 2) {
-		n = getenv("HOME");
+		n = getenv("HOME"); /* if there is 1 argument go to home dir */
 		
-		if (!n) {
+		if (!n) { /* add a / to adjoin the following argument */
 			n = "/";
 		}
 		
 	} 
 	
 	else if (argc == 2) {
-		n = argv[1];
+		n = argv[1]; /* set n to the target dir */
 	
 	} 
 	
 	else {
-		fprintf(stderr, "Too many arguments\n");
+		fprintf(stderr, "cd: too many arguments\n"); /* error if there's an invalid amount of args */
 		return -1;
 	}
 	
-	x = chdir(n);
+	x = chdir(n); /* set the directory */
 
 	if (!x) {
 		return 0;
@@ -59,50 +61,58 @@ static int cmdCd(int argc, char *const *argv) {
 
 }
 
+/* Code that performs the exit command */
 static int cmdExit(int argc, char *const *argv) {
-	if (argc < 2) {
+	if (argc < 2) { /* if there's one arg just exit gracefully */
 		exit(0);
 	} else {
-		int ret;
+		int ret; /* exit code placeholder */
 
 		if (argc > 3) {
-			fprintf(stderr, "Too many arguments\n");
+			fprintf(stderr, "exit: too many arguments\n");
 			return -1;
 		}
 
 		ret = 0;
-		sscanf(argv[1], "%d", &ret);
+		sscanf(argv[1], "%d", &ret); 
 
-		/* if sscanf fails, we exit with 0 */
+		/* if sscanf fails, exit with 0 */
 		exit(ret);
 	}
+	
 	return 0;
 }
 
+/* builtin struct */
 static const builtin_t builtins[] = {
 	{ "exit", cmdExit },
 	{ "cd", cmdCd },
 	{ NULL, cmdDef }
 };
 
-static command_t *builtin_get(builtin_t const *dict, char const *name) {
-	while (dict->name != NULL) {
+/* struct search function */
+static command_t *builtin_get(builtin_t const *dict, const char  *name) {
+	while (dict->name != NULL) { /* keep searching while there's commands */
 		if (!strcmp(dict->name, name))
 			break;
-		++dict;
+		++dict; /*move to the next command */
 	}
 	
-	return dict->func;
+	return dict->func; /* go to the function from builtin */
 }
 
 int main(int argc, char **argv) {
 	
+	
+	int place = 0;
+	
 	if (isatty(0)) { /* get terminal input, otherwise content is already in argv*/
 		char cmd[BUFSIZE];
 		
+		/* get input*/
 		printf("$: ");
-		fgets(cmd, sizeof(cmd), stdin);
-		cmd[strlen(cmd) - 1] = '\0';
+		fgets(cmd, sizeof(cmd), stdin); /* use fgets to recieve input */
+		cmd[strlen(cmd) - 1] = '\0'; /* remove the ret char at the end of line */
 
 		if(strlen(cmd) == 0) { /* if no input startover */
 			main(argc, argv);
@@ -110,28 +120,28 @@ int main(int argc, char **argv) {
 
 		/* alocate space for counter and token array */
 		int argc = 0;
-		char **argv = (char**)malloc((MAXARG+1)*sizeof(char*));
-		int i;
-		for(i = 0; i < MAXARG+1; i++) {
-			argv[i] = (char*)malloc((MAXARG+1)*sizeof(char));
-		}
-	
+		char argv[50][100];
+		
+		/* var to maintain arg place */
 		int place = 0;
 	
 		int j;
-		for(j = 0; j <= strlen(cmd); j++ ) {
+		for(j = 0; j <= strlen(cmd); j++ ) { /* cycle through each letter */
 			
-			if (argc >= MAXARG) { /* restart and deallocate space*/
+			if (argc >= MAXARG) { /* RAGE QUIT */
 				exit(0);
 			}
-			
+			/* the following section is the culmination of 30+ hours of work */
+			/* and debugging, we tried about 3 different more complicated    */
+			/* methods and non of them worked until this, Thanks Lee Zhou    */
+			/* for recommending to use "else if" - FML moment at 3:00am      */
 			if (isspace(cmd[j])) {
 				continue;
 			}
 			
 			else if (cmd[j] == '|') {
 			
-				argv[argc] = "|";
+				argv[argc][place] = '|';
 				argc++;
 				
 			}
@@ -173,35 +183,64 @@ int main(int argc, char **argv) {
 				
 			
 		}
+		
+		/* parse out any pipes */ 
+		int command_num = 0;
+		char* commands[50][100];
+		
 		int k;
-		for(k = 0; k <= argc; k++) {	
-		printf("Token %d: \"%s\"\n", k, argv[k]);
-		
-		//if (argv[argc] != "|") {
-		//	commands[command_num][place] =  argv[argc];
+		for(k = 0; k <= argc; k++) {
 			
-		//}
-		//else { 
-		//	command_num++;
-		//	place = 0;
-		//}
+			printf("Token %d: \"%s\"\n", k, argv[k]); /* print existing tokens */
 		
-		
+			if (strcmp(argv[k], "|")) {
+				commands[command_num][place] =  argv[k]; /* place token into a command */
+				place++;
 			
+			}
+			else { 
+				command_num++; /* increment the command counter */
+				place = 0; 
+			}
 		}
-			
 		
+		/* Search for existing commmands */
+		int h;
+		for (h = 0; h < command_num; h++) {
+			if (command_num > 0) {
+				builtin_get(builtins, commands[h][0])(command_num, commands[h]);
+			}
+		}
 	}
 	
-	/* parse out any pipes */ 
+	else { 
 	
-	
-	/* Search for existing commmands */
-	 if (argc > 0) {
-		argv[argc] = NULL;
-		builtin_get(builtins, argv[0])(argc, argv);
+		/* parse out any pipes */ 
+		int command_num = 0;
+		char* commands[50][100];
+		
+		int k;
+		for(k = 0; k <= argc; k++) {
+			
+			printf("Token %d: \"%s\"\n", k, argv[k]); /* print existing tokens */
+		
+			if (strcmp(argv[k], "|")) {
+				commands[command_num][place] =  argv[k]; /* place token into a command */
+				place++;
+			
+			}
+			else { 
+				command_num++; /* increment the command counter */
+				place = 0; 
+			}
+		}
+		
+		/* Search for existing commmands */
+		if (argc > 0) {
+			argv[argc] = NULL;
+			builtin_get(builtins, argv[0])(argc, argv);
+		}
 	}
-	
 	
 	return 0;
 	
